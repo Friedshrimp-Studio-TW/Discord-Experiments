521cb5fef8fca12b117e189747b43e5f5c7d3d00
VincentX0905(炸蝦) <VincentX0905@gmail.com>
2022-12-17 21:31:54 +0800

    Create DiscordExperiments.js

---
+++ DiscordExperiments.js
@@ -0,0 +1,30 @@
+/**
+ * @name DiscordExperiments
+ * @author VincentX0905
+ * @description Open Discord Experiments function
+ * @version V1.0.0
+*/
+
+module.exports = class discordExperiments {
+  start() {
+    let wpRequire;
+    window.webpackChunkdiscord_app.push([
+      [Math.random()],
+      {},
+      (req) => {
+        wpRequire = req;
+      },
+    ]);
+    let mod = Object.values(wpRequire.c).find((x) => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+    let usermod = Object.values(wpRequire.c).find((x) => x?.exports?.default?.getUsers);
+    let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes);
+    try {
+      nodes.find((x) => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
+    } catch (e) {}
+    let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+    usermod.exports.default.__proto__.getCurrentUser = () => ({ isStaff: () => true });
+    nodes.find((x) => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
+    usermod.exports.default.__proto__.getCurrentUser = oldGetUser;
+  }
+  stop() {}
+};
