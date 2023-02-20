f39c24a3e4beef3d4df52ee74c6ad42f8c6a76cd
VincentX0905(炸蝦) <VincentX0905@gmail.com>
2023-02-20 19:02:47 +0800

    Update DiscordExperiments.plugin.js

--- DiscordExperiments.plugin.js
+++ DiscordExperiments.plugin.js
@@ -2,7 +2,7 @@
  * @name DiscordExperiments
  * @author VincentX0905(炸蝦)
  * @description Open Discord Experiments function
- * @version V1.2.0
+ * @version 1.3.0
  * @authorId 909608773927202906
  * @donate https://donate.fsbot.tk
  * @invite Pw8z4YkBFB
@@ -13,33 +13,61 @@
 
 module.exports = class discordExperiments {
   start() {
-    let wpRequire;
-    window.webpackChunkdiscord_app.push([
-      [Math.random()],
-      {},
-      (req) => {
-        wpRequire = req;
-      },
-    ]);
-    let mod = Object.values(wpRequire.c).find((x) => typeof x?.exports?.Z?.isDeveloper !== "undefined");
-    let usermod = Object.values(wpRequire.c).find((x) => x?.exports?.default?.getUsers);
-    let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes);
+    BdApi.showToast("Now you use DiscordExperiments V1.2.0", {type:"info",icon: true,timeout: 7500,forceShow: true});
     try {
-      nodes.find((x) => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
-    } catch (e) {}
-    let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
-    usermod.exports.default.__proto__.getCurrentUser = () => ({ isStaff: () => true });
-    nodes.find((x) => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
-    usermod.exports.default.__proto__.getCurrentUser = oldGetUser;
+      let wpRequire;
+      window.webpackChunkdiscord_app.push([
+        [Math.random()],
+        {},
+        (req) => {
+          wpRequire = req;
+        },
+      ]);
+      let mod = Object.values(wpRequire.c).find((x) => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+      let usermod = Object.values(wpRequire.c).find((x) => x?.exports?.default?.getUsers);
+      let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes);
+      try {
+        nodes.find((x) => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
+      } catch (e) {}
+      let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+      usermod.exports.default.__proto__.getCurrentUser = () => ({ isStaff: () => true });
+      nodes.find((x) => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
+      usermod.exports.default.__proto__.getCurrentUser = oldGetUser;
+    } catch (err) {
+      BdApi.showNotice(
+        "DiscordExperiments 插件錯誤 | DiscordExperiments Plugin error",
+        {
+          type: "error",
+          buttons: [
+            {
+              label: "回報 | Report",
+              onClick: () => window.open("https://github.com/vincentwang0905/DiscordExperiments/issues", "mozillaTab")
+            }
+          ]
+        }
+      );
+      return BdApi.showNotice(
+        `錯誤 | Error: ${err}`,
+        {
+          type: "error",
+          buttons: [
+            {
+              label: "回報 | Report",
+              onClick: () => window.open("https://github.com/vincentwang0905/DiscordExperiments/issues", "mozillaTab")
+            }
+          ]
+        }
+      );
+    }
   }
 
   stop() {
-    BdApi.showNotice("You need to reload Discord for disabling Experiments", {
+    BdApi.showNotice("你需要重啟 BD 來關閉 DiscordExperiments | You need to reboot BD for disabling DiscordExperiments", {
       type: "warning",
       buttons: [{
-        label: "Reload Discord",
+        label: "重啟 BD | Reboot BD",
         onClick: () => location.reload()
       }]
     });
   }
-};
+}
