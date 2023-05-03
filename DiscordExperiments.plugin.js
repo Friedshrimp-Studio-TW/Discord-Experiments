a4a876f4f21fdd26e018733a66add1894780928e
VincentX0905(炸蝦) <VincentX0905@gmail.com>
2023-05-03 22:22:58 +0800

    Update DiscordExperiments.plugin.js

--- DiscordExperiments.plugin.js
+++ DiscordExperiments.plugin.js
@@ -1,10 +1,10 @@
 /**
  * @name DiscordExperiments
  * @author VincentX0905(炸蝦)
- * @description Open Discord Experiments function
- * @version 1.3.0
+ * @description Open Discord Experiments function | 啟用 Discord 實驗功能
+ * @version 1.4.0
  * @authorId 909608773927202906
- * @donate https://donate.fsbot.tk
+ * @donate https://donate.fsbot.xyz
  * @invite Pw8z4YkBFB
  * @website https://github.com/vincentwang0905/DiscordExperiments
  * @source https://github.com/vincentwang0905/DiscordExperiments
@@ -13,27 +13,17 @@
 
 module.exports = class discordExperiments {
   start() {
-    BdApi.showToast("Now you use DiscordExperiments V1.2.0", {type:"info",icon: true,timeout: 7500,forceShow: true});
+    BdApi.showToast("Now you use DiscordExperiments V1.4.0 | 你現在使用 DiscordExperiments V1.4.0", {type:"info",icon: true,timeout: 7500,forceShow: true});
     try {
-      let wpRequire;
-      window.webpackChunkdiscord_app.push([
-        [Math.random()],
-        {},
-        (req) => {
-          wpRequire = req;
-        },
-      ]);
-      let mod = Object.values(wpRequire.c).find((x) => typeof x?.exports?.Z?.isDeveloper !== "undefined");
-      let usermod = Object.values(wpRequire.c).find((x) => x?.exports?.default?.getUsers);
-      let nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes);
-      try {
-        nodes.find((x) => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
-      } catch (e) {}
-      let oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
-      usermod.exports.default.__proto__.getCurrentUser = () => ({ isStaff: () => true });
-      nodes.find((x) => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
-      usermod.exports.default.__proto__.getCurrentUser = oldGetUser;
+      let c = window.webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);
+      let u = c.find((x)=> x?.exports?.default?.getUsers).exports.default;
+      let m = Object.values(u._dispatcher._actionHandlers._dependencyGraph.nodes);
+      u.getCurrentUser().flags |= 1;
+      m.find((x)=>x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
+      try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {}
+      m.find((x)=>x.name === "ExperimentStore").storeDidChange()
     } catch (err) {
+      console.log(err);
       BdApi.showNotice(
         "DiscordExperiments 插件錯誤 | DiscordExperiments Plugin error",
         {
