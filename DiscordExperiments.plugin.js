5fade1a17afb44031ba77387d3fb62735bd3439d
VincentX0905(炸蝦) <VincentX0905@gmail.com>
2023-12-16 17:27:13 +0800

    Update DiscordExperiments.plugin.js

--- DiscordExperiments.plugin.js
+++ DiscordExperiments.plugin.js
@@ -2,26 +2,26 @@
  * @name DiscordExperiments
  * @author VincentX0905(炸蝦)
  * @description Open Discord Experiments function | 啟用 Discord 實驗功能
- * @version 1.4.0
- * @authorId 909608773927202906
+ * @version 1.5.0
+ * @authorId 1183208834802667555
  * @donate https://donate.fsbot.xyz
- * @invite Pw8z4YkBFB
- * @website https://github.com/vincentwang0905/DiscordExperiments
- * @source https://github.com/vincentwang0905/DiscordExperiments
- * @updateUrl https://raw.githubusercontent.com/vincentwang0905/DiscordExperiments/plugins/DiscordExperiments.plugin.js
+ * @invite myZ7u8pPe9
+ * @website https://github.com/Friedshrimp-Studio-TW/Discord-Experiments/
+ * @source https://github.com/Friedshrimp-Studio-TW/Discord-Experiments/
+ * @updateUrl https://raw.githubusercontent.com/Friedshrimp-Studio-TW/Discord-Experiments/plugins/DiscordExperiments.plugin.js
  */
 
 module.exports = class discordExperiments {
   start() {
-    BdApi.showToast("Now you use DiscordExperiments V1.4.0 | 你現在使用 DiscordExperiments V1.4.0", {type:"info",icon: true,timeout: 7500,forceShow: true});
+    BdApi.showToast("Now you use DiscordExperiments V1.5.0 | 你現在使用 DiscordExperiments V1.5.0", {type:"info",icon: true,timeout: 7500,forceShow: true});
     try {
-      let c = window.webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);
-      let u = c.find((x)=> x?.exports?.default?.getUsers).exports.default;
+      let c; webpackChunkdiscord_app.push([[Symbol()],{},r=>c=r.c]); webpackChunkdiscord_app.pop();
+      let u = Object.values(c).find(x=>x?.exports?.default?.getUsers).exports.default;
       let m = Object.values(u._dispatcher._actionHandlers._dependencyGraph.nodes);
       u.getCurrentUser().flags |= 1;
       m.find((x)=>x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
-      try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {}
-      m.find((x)=>x.name === "ExperimentStore").storeDidChange()
+      try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {};
+      m.find((x)=>x.name === "ExperimentStore").storeDidChange();
     } catch (err) {
       console.log(err);
       BdApi.showNotice(
